71ecb261506231cc4c08012d48a2cbb2bce45a02
algodoncin <105017985+algodoncin@users.noreply.github.com>
2023-10-14 00:17:24 -0600

    Added basic sidebar

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,7 +1,13 @@
 <template>
-    <div>
-        <h1>Dashboard</h1>
-    </div>
+    <v-app>
+        <v-navigation-drawer>
+            <v-list-item title="My Application" subtitle="Vuetify"></v-list-item>
+            <v-divider></v-divider>
+            <v-list-item link title="Profile"></v-list-item>
+            <v-list-item link title="Configuration"></v-list-item>
+            <v-list-item link title="Log out"></v-list-item>
+        </v-navigation-drawer>
+    </v-app>
 </template>
 <script>
 export default {
