b79b01b30cec2bcbec881cd0d2c0dfa7635073fb
algodoncin <105017985+algodoncin@users.noreply.github.com>
2023-10-15 23:21:48 -0600

    moved navbar from app.vue to dashboard view directly

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,5 +1,13 @@
 <template>
     <v-app>
+        <!-- Navbar -->
+        <v-app-bar color="blue-lighten-4" prominent>
+            <v-text-field label="Search books" class="mt-5 ml-4"></v-text-field>
+            <v-spacer></v-spacer>
+            <v-btn variant="text" icon="mdi-magnify"></v-btn>
+            <v-btn variant="text" icon="mdi-filter"></v-btn>
+            <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+        </v-app-bar>
         <v-main>
             <!-- Content --> 
             <v-container>
