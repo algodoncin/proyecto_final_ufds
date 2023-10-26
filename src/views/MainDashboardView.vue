d62c430498a9616703ad392562fcccbb94b31a1d
algodoncin <105017985+algodoncin@users.noreply.github.com>
2023-10-25 23:46:28 -0600

    Routes were modified

--- src/views/MainDashboardView.vue
+++ src/views/MainDashboardView.vue
@@ -4,10 +4,10 @@
         <v-navigation-drawer permanent color="blue-lighten-4" v-if="conf">
             <v-list-item title="My Application" subtitle="Vuetify" :height="64"></v-list-item>
             <v-divider></v-divider>
-            <v-list-item link title="Home" to="/home"></v-list-item>
-            <v-list-item link title="Profile" to="/profile"></v-list-item>
-            <v-list-item link title="Search" to="/search"></v-list-item>
-            <v-list-item link title="Configuration" to="/configuration"></v-list-item>
+            <v-list-item link title="Home" to="/dashboard/home"></v-list-item>
+            <v-list-item link title="Profile" to="/dashboard/profile"></v-list-item>
+            <v-list-item link title="Search" to="/dashboard/search"></v-list-item>
+            <v-list-item link title="Configuration" to="/dashboard/configuration"></v-list-item>
             <v-list-item link title="Log out"></v-list-item>
         </v-navigation-drawer>
         <v-main>
