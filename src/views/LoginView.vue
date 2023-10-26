74c0f7012cbc02fef6214421ac8930f98281a2a6
algodoncin <105017985+algodoncin@users.noreply.github.com>
2023-10-26 15:17:48 -0600

    Styled login and register views

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,12 +1,92 @@
 <template>
     <v-app>
         <v-container>
-            <h1>Login</h1>
+            <div>
+                <v-img
+                class="mx-auto my-6"
+                max-width="228"
+                src="https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-v3-slim-text-light.svg"
+                ></v-img>
+                <v-card
+                class="mx-auto pa-12 pb-8"
+                elevation="8"
+                max-width="448"
+                rounded="lg"
+                >
+                <div class="text-subtitle-1 text-medium-emphasis">Email</div>
+                <v-text-field
+                    density="compact"
+                    placeholder="Email address"
+                    prepend-inner-icon="mdi-email-outline"
+                    variant="outlined"
+                    v-model="user.email"
+                ></v-text-field>
+                <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+                    Password
+                    <a
+                    class="text-caption text-decoration-none text-blue"
+                    href="#"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    >
+                    Forgot login password?</a>
+                </div>
+                <v-text-field
+                    :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+                    :type="visible ? 'text' : 'password'"
+                    density="compact"
+                    placeholder="Enter your password"
+                    prepend-inner-icon="mdi-lock-outline"
+                    variant="outlined"
+                    @click:append-inner="visible = !visible"
+                    v-model="user.password"
+                ></v-text-field>
+                <v-card
+                    class="mb-12"
+                    color="surface-variant"
+                    variant="tonal"
+                >
+                </v-card>
+                <v-btn
+                    block
+                    class="mb-8"
+                    color="blue"
+                    size="large"
+                    variant="tonal"
+                    @click="login()"
+                >
+                    Log In
+                </v-btn>
+                <v-card-text class="text-center">
+                    <a
+                    class="text-blue text-decoration-none"
+                    href="/register"
+                    rel="noopener noreferrer"
+                    >
+                    Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
+                    </a>
+                </v-card-text>
+                </v-card>
+            </div>
         </v-container>
     </v-app>
 </template>
 <script>
 export default {
-    name: 'LoginView'
+    name: 'LoginView',
+    data(){
+        return{
+            user: {
+                email: '',
+                password: ''
+            },
+            visible: false,
+        }
+    },
+    methods: {
+        login(){
+            console.log(this.user.email, this.user.password);
+        }
+    }
 }
 </script>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,7 +1,72 @@
 <template>
     <v-app>
         <v-container>
-            <h1>Register</h1>
+            <div>
+                <v-img
+                class="mx-auto my-6"
+                max-width="228"
+                ></v-img>
+                <v-card
+                class="mx-auto pa-12 pb-8"
+                elevation="8"
+                max-width="448"
+                rounded="lg"
+                >   <div class="text-subtitle-1 text-medium-emphasis">Username</div>
+                    <v-text-field
+                        density="compact"
+                        placeholder="Username"
+                        prepend-inner-icon="mdi-account-outline"
+                        variant="outlined"
+                    ></v-text-field>
+                    <div class="text-subtitle-1 text-medium-emphasis">Email</div>
+                    <v-text-field
+                        density="compact"
+                        placeholder="Email address"
+                        prepend-inner-icon="mdi-email-outline"
+                        variant="outlined"
+                    ></v-text-field>
+                    <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+                        Password
+                    </div>
+                    <v-text-field
+                        :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+                        :type="visible ? 'text' : 'password'"
+                        density="compact"
+                        placeholder="Enter your password"
+                        prepend-inner-icon="mdi-lock-outline"
+                        variant="outlined"
+                        @click:append-inner="visible = !visible"
+                    ></v-text-field>
+                    <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+                        Confirm password
+                    </div>
+                    <v-text-field
+                        :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+                        :type="visible ? 'text' : 'password'"
+                        density="compact"
+                        placeholder="Confirm your password"
+                        prepend-inner-icon="mdi-lock-outline"
+                        variant="outlined"
+                        @click:append-inner="visible = !visible"
+                    ></v-text-field>
+                    <v-card
+                        class="mb-12"
+                        color="surface-variant"
+                        variant="tonal"
+                    >
+                    </v-card>
+                    <v-btn
+                        block
+                        class="mb-8"
+                        color="success"
+                        size="large"
+                        variant="tonal"
+                        to="/login"
+                    >
+                        Sign up 
+                    </v-btn>
+                </v-card>
+            </div>
         </v-container>
     </v-app>
 </template>
