10741ddb4e476fe03aff6cf2e8d69bd617464841
algodoncin <105017985+algodoncin@users.noreply.github.com>
2023-10-26 16:34:59 -0600

    Started to work on registration validation

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,7 +1,7 @@
 <template>
     <v-app>
         <v-container>
-            <div>
+            <form @submit="register">
                 <v-img
                 class="mx-auto my-6"
                 max-width="228"
@@ -17,6 +17,7 @@
                         placeholder="Username"
                         prepend-inner-icon="mdi-account-outline"
                         variant="outlined"
+                        v-model="user.username"
                     ></v-text-field>
                     <div class="text-subtitle-1 text-medium-emphasis">Email</div>
                     <v-text-field
@@ -24,6 +25,7 @@
                         placeholder="Email address"
                         prepend-inner-icon="mdi-email-outline"
                         variant="outlined"
+                        v-model="user.email"
                     ></v-text-field>
                     <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
                         Password
@@ -36,19 +38,27 @@
                         prepend-inner-icon="mdi-lock-outline"
                         variant="outlined"
                         @click:append-inner="visible = !visible"
+                        v-model="user.password"
                     ></v-text-field>
                     <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
                         Confirm password
                     </div>
                     <v-text-field
-                        :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-                        :type="visible ? 'text' : 'password'"
+                        :append-inner-icon="visible1 ? 'mdi-eye-off' : 'mdi-eye'"
+                        :type="visible1 ? 'text' : 'password'"
                         density="compact"
                         placeholder="Confirm your password"
                         prepend-inner-icon="mdi-lock-outline"
                         variant="outlined"
-                        @click:append-inner="visible = !visible"
+                        @click:append-inner="visible1 = !visible1"
+                        v-model="user.passwordVerification"
                     ></v-text-field>
+                    <v-alert
+                        type="error"
+                        title="Passwords not matching"
+                        text="The passwords have to match."
+                        v-if="passwordMatch"
+                    ></v-alert>
                     <v-card
                         class="mb-12"
                         color="surface-variant"
@@ -61,17 +71,46 @@
                         color="success"
                         size="large"
                         variant="tonal"
-                        to="/login"
+                        @click="register"
                     >
                         Sign up 
                     </v-btn>
                 </v-card>
-            </div>
+            </form>
         </v-container>
     </v-app>
 </template>
 <script>
 export default {
-    name: 'LoginView'
+    name: 'LoginView',
+    data(){
+        return{
+            user: {
+                username: '',
+                email: '',
+                password: '',
+                passwordVerification: ''
+            },
+            passwordMatch: false,
+            visible: false,
+            visible1: false,
+        }
+    },
+    methods: {
+        register(){
+            const password1 = this.user.password;
+            const password2 = this.user.passwordVerification;
+
+            if(password1 == password2){
+                console.log(this.user.username, this.user.email, this.user.password);
+                this.user = {};
+                this.passwordMatch = false;
+            }else{
+                this.passwordMatch = true
+            }
+
+        }
+    },
 }
+
 </script>
