dd5d73ea3d209f7d0744de7fabb8931f184f2cad
algodoncin <105017985+algodoncin@users.noreply.github.com>
2023-10-26 15:54:53 -0600

    Modified home view CSS with vuetify

--- src/views/DashboardHomeView.vue
+++ src/views/DashboardHomeView.vue
@@ -3,49 +3,10 @@
         <v-main>
             <!-- Content --> 
             <v-container>
-                <v-grid>
-                    <v-row>
-                        <v-col>
-                            <v-card>
-                                <v-card-title>NombreCuaderno</v-card-title>
-                                <v-card-subtitle>NombreAutor</v-card-subtitle>
-                                <v-card-text>
-                                    Test text
-                                </v-card-text>
-                                <v-card-actions>
-                                    <v-btn>Read</v-btn>
-                                    <v-btn>Write</v-btn>
-                                </v-card-actions>
-                            </v-card>
-                        </v-col>
-                        <v-col>
-                            <v-card>
-                                <v-card-title>NombreCuaderno</v-card-title>
-                                <v-card-subtitle>NombreAutor</v-card-subtitle>
-                                <v-card-text>
-                                    Test text
-                                </v-card-text>
-                                <v-card-actions>
-                                    <v-btn>Read</v-btn>
-                                    <v-btn>Write</v-btn>
-                                </v-card-actions>
-                            </v-card>
-                        </v-col>
-                        <v-col>
-                            <v-card>
-                                <v-card-title>NombreCuaderno</v-card-title>
-                                <v-card-subtitle>NombreAutor</v-card-subtitle>
-                                <v-card-text>
-                                    Test text
-                                </v-card-text>
-                                <v-card-actions>
-                                    <v-btn>Read</v-btn>
-                                    <v-btn>Write</v-btn>
-                                </v-card-actions>
-                            </v-card>
-                        </v-col>
-                        <v-col>
-                            <v-card>
+                <v-grid >
+                    <v-row class="mt-3">
+                        <v-col cols="3">
+                            <v-card >
                                 <v-card-title>NombreCuaderno</v-card-title>
                                 <v-card-subtitle>NombreAutor</v-card-subtitle>
                                 <v-card-text>
@@ -60,7 +21,7 @@
                     </v-row>
                 </v-grid>
                 <div>
-                    <v-btn icon="mdi-plus" size="x-large" class="bg-info" to="/dashboard/create"></v-btn>
+                    <v-btn icon="mdi-plus" size="x-large" class="bg-info " to="/dashboard/create" vertical="bottom" horizontal="end"></v-btn>
                 </div>
                 
             </v-container>
