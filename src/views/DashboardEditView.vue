2bf371dfb004cd650282d4220c20160b38453372
algodoncin <105017985+algodoncin@users.noreply.github.com>
2023-11-08 16:58:34 -0600

    Notebook info is given to edit page

--- src/views/DashboardEditView.vue
+++ src/views/DashboardEditView.vue
@@ -9,18 +9,43 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     name: 'DashboardCreateView',
     data(){
         return{
-
+            currentId: this.$store.state.user.id,
+            currentToken: this.$store.state.token,
+            currentNotebookId: this.$route.params.id,
+            notebook: {}
         }
     },
     methods: {
-
+        getCurrentNotebook(notebookId){
+            axios.get(`http://localhost:2046/api/notebook/one/${notebookId}`, {
+                headers: {
+                    Authorization: this.currentToken
+                }
+            })
+            .then((respuesta)=>{
+                if(respuesta.status == 200){
+                    let res = respuesta.data.notebook;
+                    this.notebook = res;
+                    console.log(this.notebook);
+                }
+            })
+            .catch((err)=>{
+                console.log(`Ocurrio un error ${err}`);
+            })
+        }
+    },
+    beforeCreate(){
+        
     },
     created(){
-        console.log(this.$route.params);
+        console.log(this.currentNotebookId);
+        this.getCurrentNotebook(this.currentNotebookId)
     }
 }
 
